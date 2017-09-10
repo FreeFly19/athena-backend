02b2d702df1c2543b1a6404c0560348e58fbbb61
oleksandrm <oleksandrm@interlink-ua.com>
2017-09-10 17:16:55 +0300

    Added infrastructure files

---
+++ src/main/java/com/athena/AthenaBackendApplication.java
@@ -0,0 +1,12 @@
+package com.athena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenaBackendApplication.class, args);
+	}
+}
